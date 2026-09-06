39c1bde8e9d4841ac3f727a576a98b429fd47043
Ter_jeff <loco0402@yahoo.com.tw>
2026-09-06 23:16:27 +0800

    U

--- src/SltCsharpMetrics.Tests/CommandLineTests.cs
+++ src/SltCsharpMetrics.Tests/CommandLineTests.cs
@@ -6,7 +6,7 @@ namespace SltCsharpMetrics.Tests;
 public class CommandLineTests
 {
     // Real-world solution used as the command-line tool's input for these tests.
-    private const string CommonSlnPath = @"C:\GitHub\TrainingProgram\Common.sln";
+    private const string CommonSlnPath = @"C:\GitHub\test\Automation.sln";
 
     private string _outputDirectory = null!;
     private string _outputPath = null!;
